55610e9f930e80274eaeb887def13ed5e955d21f
hamish <hamishgeorgedavie@gmail.com>
2023-03-31 13:47:01 +1000

    Additional changes

--- pokedex.js
+++ pokedex.js
@@ -3,7 +3,19 @@ const pokemonCount = 151;
 var pokedex = {}; // {1 : {"name" : "bulbsaur", "img" : url, "type" : ["grass", "poison"], "desc" : "...."} }
 
 window.onload = async function() {
-    getPokemon(1);
+    // getPokemon(1);
+    for (let i = 1; i <= pokemonCount; i ++) {
+        await getPokemon(i);
+        //<div id="1" class="pokemon-name">BULBASAUR</div>
+    let pokemon = document.createElement("div");
+    pokemon.id = i;
+    pokemon.innerText = i.toString() + ". " + pokedex[i]["name"].toUpperCase();
+    pokemon.classList.add("pokemon-name");
+    document.getElementById("pokemon-list").append(pokemon);
+
+    }
+
+    console.log(pokedex);
 }
 
 async function getPokemon(num) {
@@ -16,6 +28,14 @@ async function getPokemon(num) {
     let pokemonName = pokemon["name"];
     let pokemonType = pokemon["types"];
     let pokemonImg = pokemon["sprites"]["front_default"];
+
+    res = await fetch(pokemon ["species"]["url"]);
+    let pokemonDesc = await res.json();
+
+    // console.log(pokemonDesc);
+    pokemonDesc = pokemonDesc["flavor_text_entries"][9]["flavor_text"]
+
+    pokedex[num] = {"name" : pokemonName, "img" : pokemonImg, "types" : pokemonType, "desc" : pokemonDesc}
 }
 
 
