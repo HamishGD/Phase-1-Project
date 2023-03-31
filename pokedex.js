1ee40142b7c0da97d92434a6e3529078f5aa0c17
hamish <hamishgeorgedavie@gmail.com>
2023-03-31 12:36:34 +1000

    Some js added

--- pokedex.js
+++ pokedex.js
@@ -1,3 +1,23 @@
 
+const pokemonCount = 151;
+var pokedex = {}; // {1 : {"name" : "bulbsaur", "img" : url, "type" : ["grass", "poison"], "desc" : "...."} }
+
+window.onload = async function() {
+    getPokemon(1);
+}
+
+async function getPokemon(num) {
+    let url = "https://pokeapi.co/api/v2/pokemon/" + num.toString();
+
+    let res = await fetch(url);
+    let pokemon = await res.json();
+    // console.log(pokemon);
+
+    let pokemonName = pokemon["name"];
+    let pokemonType = pokemon["types"];
+    let pokemonImg = pokemon["sprites"]["front_default"];
+}
+
+
 
 
