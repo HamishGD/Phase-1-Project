12675794e461467bb9f8c0d6982f213ca7bcba08
hamish <hamishgeorgedavie@gmail.com>
2023-03-31 11:20:59 +1000

    Changes

--- index.js
+++ index.js
@@ -1,25 +1,3 @@
-let darkButton = document.getElementById('darkMode');
-let rndBtn = document.getElementId('randomBtn');
-let dogBtn = document.getElementById('dogFact');
-console.log(darkButton);
 
-darkButton.addEventListener('click', toggleLightMode);
-rndBtn.addEventListener('click', randomButtonMove);
-dogBtn.addEventListener('click', fetchFacts);
 
-let lightModeOn = true;
-
-    function toggleLightMode(){
-        const body = document.querySelector('body');
-        console.log("Light Mode Toggle started");
-        if(lightModeOn == true){
-            console.log(lightModeOn)
-
-            lightModeOn = false;
-            body.setAttribute("id", "lightModeOn");
-            console.log("LightModeOn");
-            return;
-        }}
-
-        
 
