19f7a2688f2bed53b99c45382585c1f36aa4f072
hamish <hamishgeorgedavie@gmail.com>
2023-04-02 19:39:24 +1000

    JS first img works

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //const pokemonCount = 151;
 
 //DOM Objects
+const mainScreen = document.querySelector('.main-screen');
 const pokeName = document.querySelector('.poke-name');
 const pokeId = document.querySelector('.poke-id');
 const pokeFrontImage = document.querySelector('.poke-front-image');
@@ -9,6 +10,7 @@ const pokeTypeOne = document.querySelector('.poke-type-one');
 const pokeTypeTwo = document.querySelector('.poke-type-two');
 const pokeWeight = document.querySelector('.poke-weight');
 const pokeHeight = document.querySelector('.poke-height');
+const pokeMove = document.querySelector('.poke-move');
 
 console.log(pokeName);
 
@@ -21,9 +23,34 @@ fetch('https://pokeapi.co/api/v2/pokemon/1')
 .then(res => res.json())
 .then(data => {
     console.log(data);
-    
+    mainScreen.classList.remove('hide');
     pokeName.textContent = data['name'];
+    pokeId.textContent = data['id'];
+    pokeWeight.textContent = data['weight'];
+    pokeHeight.textContent = data['height'];
+    pokeMove.textContent = data['move'];
+
+    const dataTypes = data['types'];
+    const dataFirstType = dataTypes[0];
+    const dataSecondType = dataTypes[1];
+    pokeTypeOne.textContent = dataFirstType['type']['name'];
+    if (dataSecondType) {
+        pokeTypeTwo.classList.remove('hide');
+        pokeTypeTwo.textContent = dataSecondType['type']['name'];
+    } else {
+        pokeTypeTwo.classList.add('hide');
+        pokeTypeTwo.textContent = '';
+    }
+
+    //update img src for pokemon
+    pokeFrontImage.src = data['sprites']['front_default'] || ''; 
+    pokeBackImage.src = data['sprites']['back_default'] || ''; //short circuit operator with empty string (in case of null) as backup
 
 
 
-});
+    });
+
+
+    
+
+
