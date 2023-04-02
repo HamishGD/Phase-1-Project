780e5594f76a2d8db5eede701c980a3bc979d21b
hamish <hamishgeorgedavie@gmail.com>
2023-04-02 15:11:24 +1000

    JS test

--- app.js
+++ app.js
@@ -5,3 +5,20 @@ const pokeName = document.querySelector('poke-name');
 const pokeId = document.querySelector('poke-id');
 const pokeFrontImage = document.querySelector('poke-front-image');
 const pokeBackImage = document.querySelector('poke-back-image');
+const pokeTypeOne = document.querySelector('poke-type-one');
+const pokeTypeTwp = document.querySelector('poke-type-two');
+const pokeWeight = document.querySelector('poke-weight');
+const pokeHeight = document.querySelector('poke-height');
+
+console.log(pokeName);
+
+fetch('https://pokeapi.co/api/v2/pokemon/1')
+.then(res => res.json())
+.then(data => {
+    console.log(data);
+    mainScreen.classList.remove('hide');
+    pokeName.textContent = data['name'];
+    pokeId.textContent = data['id'];
+});
+
+
