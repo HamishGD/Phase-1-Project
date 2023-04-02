d1076ac08b110b7ed1b3cd4e2a799cb973f03cbd
hamish <hamishgeorgedavie@gmail.com>
2023-04-03 00:45:29 +1000

    Js complete

--- app.js
+++ app.js
@@ -8,8 +8,7 @@ const pokeTypeOne = document.querySelector('.poke-type-one');
 const pokeTypeTwo = document.querySelector('.poke-type-two');
 const pokeWeight = document.querySelector('.poke-weight');
 const pokeHeight = document.querySelector('.poke-height');
-const pokeMove = document.querySelector('.poke-move');
-const pokeListItems = document.querySelector('.list-item');
+const pokeListItems = document.querySelectorAll('.list-item');
 const leftButton = document.querySelector('.left-button');
 const rightButton = document.querySelector('.right-button');
 
@@ -25,6 +24,9 @@ let nextUrl = null;
 
 
 //FUNCTIONS
+//capitalize all words first letter
+const capitalize = (str) => str[0].toUpperCase() + str.substr(1);
+
 //to reset screen after each user 'click on a pokemon' interaction
 const resetScreen = () => {
     mainScreen.classList.remove('hide');
@@ -32,19 +34,38 @@ const resetScreen = () => {
        mainScreen.classList.remove(type);
     }
 };
-const handleRightButtonClick = () => {}
 
-//EVENT LISTENERS
-leftButton.addEventListener('click', );
-rightButton.addEventListener('click', );
+const fetchPokeList = url => {
+    fetch (url)
+    .then(res => res.json())
+    .then(data => {
+        const { results, previous, next } = data;
+        prevUrl = previous;
+        nextUrl = next;
+    
+    //for loop
+    for (let i = 0; i < pokeListItems.length ; i++) {
+        const pokeListItem = pokeListItems[i];
+        const resultData  = results[i];
 
-//get data for left side of screen
-//URL API
-fetch('https://pokeapi.co/api/v2/pokemon/1')
+        if (resultData) {
+            const { name, url } = resultData;
+            const urlArray = url.split('/');
+            //if result data exists then we can restructure it cause it has 'name' and 'url' properties
+            const id = urlArray[urlArray.length - 2];
+            pokeListItem.textContent = id + '. ' + capitalize(name);
+        } else {
+            pokeListItem.textContent = '';
+        }
+       }
+    });
+};
+
+const fetchPokeData = id => {
+fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
 .then(res => res.json())
 .then(data => {
-resetScreen();
-
+    resetScreen();
 
 
 //DATA TYPES
@@ -52,12 +73,11 @@ resetScreen();
     const dataTypes = data['types'];
     const dataFirstType = dataTypes[0];
     const dataSecondType = dataTypes[1];
-    pokeTypeOne.textContent = dataFirstType['type']['name'];
+    pokeTypeOne.textContent = capitalize(dataFirstType['type']['name']);
     if (dataSecondType) {
-        //hide the stat screen initially and then show after user click interaction
         //if pokemon has a second type it will be true, if they don't then it will be false, and remove 'second type' stat bubble and 'hide it' 
         pokeTypeTwo.classList.remove('hide');
-        pokeTypeTwo.textContent = dataSecondType['type']['name'];
+        pokeTypeTwo.textContent = capitalize(dataSecondType['type']['name']);
     } else {
         pokeTypeTwo.classList.add('hide');
         pokeTypeTwo.textContent = '';
@@ -65,44 +85,49 @@ resetScreen();
     //adds in background color of pokemon dependent on type 
     mainScreen.classList.add(dataFirstType['type']['name']);
 
-    pokeName.textContent = data['name'];
+    pokeName.textContent = capitalize(data['name']);
     //basically stating that I want the id to become a string and utilise the unique 3 digit pokemon number (and if it doesn't have one, it will insert 1-2 zeros instead)
     pokeId.textContent = '#' + data['id'].toString().padStart(3, '0');
     pokeWeight.textContent = data['weight'];
     pokeHeight.textContent = data['height'];
-    pokeMove.textContent = data['move'];
-
     //update img src for pokemon
     pokeFrontImage.src = data['sprites']['front_default'] || ''; 
     pokeBackImage.src = data['sprites']['back_default'] || ''; //short circuit operator with empty string (in case of null) as backup
     });
+};
 
-    //get data for right side of screen
-    fetch('https://pokeapi.co/api/v2/pokemon?offset=0&limit=20')
-    .then(res => res.json())
-    .then(data => {
-        console.log(data);
-        const {results} = data;
+//fetch next url link upon click of button (next pokemon in order)
+const handleLeftButtonClick = () => {
+    if (prevUrl) {
+        fetchPokeList(prevUrl);
+    }
+};
 
-        //for loop
-        for (let i = 0; i < pokeListItems.length ; i++) {
-            const pokeListItem = pokeListItems[i];
-            const resultData = results[i];
-            const {name} = resultData;
+const handleRightButtonClick = () => {
+    if (nextUrl) {
+        fetchPokeList(nextUrl);
+    }
+};
 
-            //if result data exists then we can restructure it cause it has 'name' and 'url' properties
-            if (resultData) {
-                const { name, url} = resultData;
-                const urlArray = url.split('/');
-                const id = urlArray[urlArray.length -2];
-                pokeListItem.textContent = id + '.' + (name);
-            } else {
-            //if doesn't show up will return an empty string
-                pokeListItem.textContent = '';
-            }
-        }
-    });
+const handleListItemClick = (e) => {
+    if (!e.target) return;
 
+const listItem = e.target;
+if (!listItem.textContent) return;
+
+const id = listItem.textContent.split('.')[0];
+fetchPokeData(id);
+};
+
+//EVENT LISTENERS
+leftButton.addEventListener('click', handleLeftButtonClick);
+rightButton.addEventListener('click', handleRightButtonClick);
+for (const pokeListItem of pokeListItems) {
+    pokeListItem.addEventListener('click', handleListItemClick);
+}
+
+//Initialize App
+fetchPokeList('https://pokeapi.co/api/v2/pokemon?offset=0&limit=20');
 
     
 
