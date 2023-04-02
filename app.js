72e2c813766c56efa995d9965bc6dd7cf9026fcf
hamish <hamishgeorgedavie@gmail.com>
2023-04-02 21:33:02 +1000

    Js added

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //const pokemonCount = 151;
 
-//DOM Objects
+//DOM OBJECTS
 const mainScreen = document.querySelector('.main-screen');
 const pokeName = document.querySelector('.poke-name');
 const pokeId = document.querySelector('.poke-id');
@@ -11,43 +11,92 @@ const pokeTypeTwo = document.querySelector('.poke-type-two');
 const pokeWeight = document.querySelector('.poke-weight');
 const pokeHeight = document.querySelector('.poke-height');
 const pokeMove = document.querySelector('.poke-move');
-
-console.log(pokeName);
-
+const pokeListItems = document.querySelector('.list-item');
 
 
 
 
+
+//VARIABLES AND CONSTANTS ()
+const TYPES = [
+    'normal', 'grass', 'fire', 'water', 'fighting', 'flying',
+    'poison', 'ground', 'rock', 'bug', 'ghost', 'steel',
+    'electric', 'psychic', 'ice', 'dark', 'dragon', 'fairy'
+];
+
+
+
+//FUNCTIONS
+//to reset screen after each user 'click on a pokemon' interaction
+const resetScreen = () => {
+    mainScreen.classList.remove('hide');
+    for (const type of TYPES) {
+       mainScreen.classList.remove(type);
+    }
+};
+
+//get data for left side of screen
+//URL API
 fetch('https://pokeapi.co/api/v2/pokemon/1')
-
 .then(res => res.json())
 .then(data => {
-    console.log(data);
-    mainScreen.classList.remove('hide');
-    pokeName.textContent = data['name'];
-    pokeId.textContent = data['id'];
-    pokeWeight.textContent = data['weight'];
-    pokeHeight.textContent = data['height'];
-    pokeMove.textContent = data['move'];
+resetScreen();
 
+
+
+//DATA TYPES
+    //to initalise first pokemon type (0) and then if they have a second type (1)
     const dataTypes = data['types'];
     const dataFirstType = dataTypes[0];
     const dataSecondType = dataTypes[1];
     pokeTypeOne.textContent = dataFirstType['type']['name'];
     if (dataSecondType) {
+        //hide the stat screen initially and then show after user click interaction
+        //if pokemon has a second type it will be true, if they don't then it will be false, and remove 'second type' stat bubble and 'hide it' 
         pokeTypeTwo.classList.remove('hide');
         pokeTypeTwo.textContent = dataSecondType['type']['name'];
     } else {
         pokeTypeTwo.classList.add('hide');
         pokeTypeTwo.textContent = '';
     }
+    //adds in background color of pokemon dependent on type 
+    mainScreen.classList.add(dataFirstType['type']['name']);
+
+    pokeName.textContent = data['name'];
+    //basically stating that I want the id to become a string and utilise the unique 3 digit pokemon number (and if it doesn't have one, it will insert 1-2 zeros instead)
+    pokeId.textContent = '#' + data['id'].toString().padStart(3, '0');
+    pokeWeight.textContent = data['weight'];
+    pokeHeight.textContent = data['height'];
+    pokeMove.textContent = data['move'];
 
     //update img src for pokemon
     pokeFrontImage.src = data['sprites']['front_default'] || ''; 
     pokeBackImage.src = data['sprites']['back_default'] || ''; //short circuit operator with empty string (in case of null) as backup
+    });
 
+    //get data for right side of screen
+    fetch('https://pokeapi.co/api/v2/pokemon?offset=0&limit=20')
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        const {results} = data;
 
+        //for loop
+        for (let i = 0; i < pokeListItems.length ; i++) {
+            const pokeListItem = pokeListItems[i];
+            const resultData = results[i];
+            const {name} = resultData;
 
+            //if result data exists then we can restructure it cause it has 'name' and 'url' properties
+            if (resultData) {
+                pokeListItem.textContent = name;
+            } else {
+            //if doesn't show up will return an empty string
+                pokeListItem.textContent = '';
+
+            
+            }
+        }
     });
 
 
