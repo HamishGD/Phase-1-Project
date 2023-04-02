feab57fdf1cd4fb60645ad311cf062b5b1c3ddcf
hamish <hamishgeorgedavie@gmail.com>
2023-04-02 15:06:20 +1000

    Js entered

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 const pokemonCount = 151;
 
+const mainScreen = document.querySelector('.main-screen');
+const pokeName = document.querySelector('poke-name');
+const pokeId = document.querySelector('poke-id');
+const pokeFrontImage = document.querySelector('poke-front-image');
+const pokeBackImage = document.querySelector('poke-back-image');
